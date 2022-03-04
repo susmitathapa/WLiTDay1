51196c6129751857d90fc06a8b355718f7206cb1
susmitathapa412@gmail.com <susmitathapa412@gmail.com>
2022-03-04 22:29:43 +0545

    First assignment is done

---
+++ largest.js
@@ -0,0 +1,15 @@
+let a = 5
+let b = 10
+let c = 6
+if (a>b && a>c)
+{ 
+    console.log("a is largest")
+}
+else if (b>a && b>c)
+{
+    console.log("b is largest")
+}
+else
+{
+    console.log("c is largest")
+}
